f1d8b80eb1402565d07844730f36ecbfea3cf50c
Muhammad Turkmen <muhammadturkmen204@gmail.com>
2024-02-08 09:44:11 +0430

    init

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ toggleBtn.addEventListener('click', function() {
     if(sidebar.classList.contains('show-sidebar')) {
         sidebar.classList.remove('show-sidebar')
     }
+    else {
+        sidebar.classList.add('show-sidebar')
+    }
 })
